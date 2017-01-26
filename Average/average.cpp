7c63ec4aa49b6d032d7618360895751fe629330e
Pinterf <Pinterf@gmail.com>
2017-01-26 13:57:24 +0100

    Fix: int_sse2: add rounding. New: faster special case for 2 or 3 clips

--- Average/average.cpp
+++ Average/average.cpp
@@ -81,6 +81,7 @@ static inline void weighted_average_sse2(uint8_t *dstp, int dst_pitch, const uin
     }
 }
 
+template<int frames_count_2_3_more>
 static inline void weighted_average_int_sse2(uint8_t *dstp, int dst_pitch, const uint8_t **src_pointers, int *src_pitches, float *weights, int frames_count, int width, int height) {
     int16_t *int_weights = reinterpret_cast<int16_t*>(alloca(frames_count*sizeof(int16_t)));
     for (int i = 0; i < frames_count; ++i) {
@@ -89,63 +90,142 @@ static inline void weighted_average_int_sse2(uint8_t *dstp, int dst_pitch, const
     int mod8_width = width / 8 * 8;
     __m128i zero = _mm_setzero_si128();
 
-    for (int y = 0; y < height; ++y) {
+    __m128i round_mask = _mm_set1_epi32(0x2000);
+    __m128i round_mask2 = _mm_set_epi32(0x0000,0x2000,0x0000,0x2000);
+
+    bool even_frames = (frames_count % 2 != 0);
+
+    if (frames_count_2_3_more == 2 || frames_count_2_3_more == 3) {
+      for (int y = 0; y < height; ++y) {
         for (int x = 0; x < mod8_width; x += 8) {
-            __m128i acc_lo = _mm_setzero_si128();
-            __m128i acc_hi = _mm_setzero_si128();
+          __m128i acc_lo = _mm_setzero_si128();
+          __m128i acc_hi = _mm_setzero_si128();
 
-            for (int i = 0; i < frames_count-1; i+=2) {
-                __m128i src = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(src_pointers[i]+x));
-                __m128i src2 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(src_pointers[i+1]+x));
-                __m128i weight = _mm_set1_epi32(*reinterpret_cast<int*>(int_weights + i));
+          __m128i src = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(src_pointers[0] + x));
+          __m128i src2 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(src_pointers[1] + x));
+          __m128i weight = _mm_set1_epi32(*reinterpret_cast<int*>(int_weights));
 
-                src = _mm_unpacklo_epi8(src, zero);
-                src2 = _mm_unpacklo_epi8(src2, zero);
-                __m128i src_lo = _mm_unpacklo_epi16(src, src2);
-                __m128i src_hi = _mm_unpackhi_epi16(src, src2);
+          src = _mm_unpacklo_epi8(src, zero);
+          src2 = _mm_unpacklo_epi8(src2, zero);
+          __m128i src_lo = _mm_unpacklo_epi16(src, src2);
+          __m128i src_hi = _mm_unpackhi_epi16(src, src2);
 
-                __m128i weighted_lo = _mm_madd_epi16(src_lo, weight);
-                __m128i weighted_hi = _mm_madd_epi16(src_hi, weight);
+          __m128i weighted_lo = _mm_madd_epi16(src_lo, weight);
+          __m128i weighted_hi = _mm_madd_epi16(src_hi, weight);
 
-                acc_lo = _mm_add_epi32(acc_lo, weighted_lo);
-                acc_hi = _mm_add_epi32(acc_hi, weighted_hi);
-            }
+          weighted_lo = _mm_add_epi32(weighted_lo, round_mask);
+          weighted_hi = _mm_add_epi32(weighted_hi, round_mask);
 
-            if (frames_count % 2 != 0) {
-                __m128i src = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(src_pointers[frames_count-1]+x));
-                __m128i weight = _mm_set1_epi32(int_weights[frames_count - 1]);
+          acc_lo = _mm_add_epi32(acc_lo, weighted_lo);
+          acc_hi = _mm_add_epi32(acc_hi, weighted_hi);
 
-                src = _mm_unpacklo_epi8(src, zero);
-                __m128i src_lo = _mm_unpacklo_epi16(src, zero);
-                __m128i src_hi = _mm_unpackhi_epi16(src, zero);
+          if (frames_count_2_3_more == 3) {
+            __m128i src = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(src_pointers[2] + x));
+            __m128i weight = _mm_set1_epi32(int_weights[2]);
 
-                __m128i weighted_lo = _mm_madd_epi16(src_lo, weight);
-                __m128i weighted_hi = _mm_madd_epi16(src_hi, weight);
+            src = _mm_unpacklo_epi8(src, zero);
+            __m128i src_lo = _mm_unpacklo_epi16(src, zero);
+            __m128i src_hi = _mm_unpackhi_epi16(src, zero);
 
-                acc_lo = _mm_add_epi32(acc_lo, weighted_lo);
-                acc_hi = _mm_add_epi32(acc_hi, weighted_hi);
-            }
+            __m128i weighted_lo = _mm_madd_epi16(src_lo, weight);
+            __m128i weighted_hi = _mm_madd_epi16(src_hi, weight);
 
-            __m128i dst_lo = _mm_srai_epi32(acc_lo, 14);
-            __m128i dst_hi = _mm_srai_epi32(acc_hi, 14);
+            weighted_lo = _mm_add_epi32(weighted_lo, round_mask2);
+            weighted_hi = _mm_add_epi32(weighted_hi, round_mask2);
 
-            __m128i dst = _mm_packs_epi32(dst_lo, dst_hi);
-            dst = _mm_packus_epi16(dst, zero);
-            _mm_storel_epi64(reinterpret_cast<__m128i*>(dstp+x), dst);
+            acc_lo = _mm_add_epi32(acc_lo, weighted_lo);
+            acc_hi = _mm_add_epi32(acc_hi, weighted_hi);
+          }
+
+          __m128i dst_lo = _mm_srai_epi32(acc_lo, 14);
+          __m128i dst_hi = _mm_srai_epi32(acc_hi, 14);
+
+          __m128i dst = _mm_packs_epi32(dst_lo, dst_hi);
+          dst = _mm_packus_epi16(dst, zero);
+          _mm_storel_epi64(reinterpret_cast<__m128i*>(dstp + x), dst);
         }
 
         for (int x = mod8_width; x < width; ++x) {
-            float acc = 0;
-            for (int i = 0; i < frames_count; ++i) {
-                acc += src_pointers[i][x] * weights[i];
-            }
-            dstp[x] = static_clip<0, 255>(acc);
+          float acc = 0;
+          acc += src_pointers[0][x] * weights[0];
+          acc += src_pointers[1][x] * weights[1];
+          if (frames_count_2_3_more == 3)
+            acc += src_pointers[2][x] * weights[2];
+          dstp[x] = static_clip<0, 255>(acc);
+        }
+       
+        src_pointers[0] += src_pitches[0];
+        src_pointers[1] += src_pitches[1];
+        if (frames_count_2_3_more == 3)
+          src_pointers[2] += src_pitches[2];
+        dstp += dst_pitch;
+      }
+    } else {
+      // generic path
+      for (int y = 0; y < height; ++y) {
+        for (int x = 0; x < mod8_width; x += 8) {
+          __m128i acc_lo = _mm_setzero_si128();
+          __m128i acc_hi = _mm_setzero_si128();
+
+          for (int i = 0; i < frames_count - 1; i += 2) {
+            __m128i src = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(src_pointers[i] + x));
+            __m128i src2 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(src_pointers[i + 1] + x));
+            __m128i weight = _mm_set1_epi32(*reinterpret_cast<int*>(int_weights + i));
+
+            src = _mm_unpacklo_epi8(src, zero);
+            src2 = _mm_unpacklo_epi8(src2, zero);
+            __m128i src_lo = _mm_unpacklo_epi16(src, src2);
+            __m128i src_hi = _mm_unpackhi_epi16(src, src2);
+
+            __m128i weighted_lo = _mm_madd_epi16(src_lo, weight);
+            __m128i weighted_hi = _mm_madd_epi16(src_hi, weight);
+
+            weighted_lo = _mm_add_epi32(weighted_lo, round_mask);
+            weighted_hi = _mm_add_epi32(weighted_hi, round_mask);
+
+            acc_lo = _mm_add_epi32(acc_lo, weighted_lo);
+            acc_hi = _mm_add_epi32(acc_hi, weighted_hi);
+          }
+
+          if (even_frames) {
+            __m128i src = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(src_pointers[frames_count - 1] + x));
+            __m128i weight = _mm_set1_epi32(int_weights[frames_count - 1]);
+
+            src = _mm_unpacklo_epi8(src, zero);
+            __m128i src_lo = _mm_unpacklo_epi16(src, zero);
+            __m128i src_hi = _mm_unpackhi_epi16(src, zero);
+
+            __m128i weighted_lo = _mm_madd_epi16(src_lo, weight);
+            __m128i weighted_hi = _mm_madd_epi16(src_hi, weight);
+
+            weighted_lo = _mm_add_epi32(weighted_lo, round_mask2);
+            weighted_hi = _mm_add_epi32(weighted_hi, round_mask2);
+
+            acc_lo = _mm_add_epi32(acc_lo, weighted_lo);
+            acc_hi = _mm_add_epi32(acc_hi, weighted_hi);
+          }
+
+          __m128i dst_lo = _mm_srai_epi32(acc_lo, 14);
+          __m128i dst_hi = _mm_srai_epi32(acc_hi, 14);
+
+          __m128i dst = _mm_packs_epi32(dst_lo, dst_hi);
+          dst = _mm_packus_epi16(dst, zero);
+          _mm_storel_epi64(reinterpret_cast<__m128i*>(dstp + x), dst);
+        }
+
+        for (int x = mod8_width; x < width; ++x) {
+          float acc = 0;
+          for (int i = 0; i < frames_count; ++i) {
+            acc += src_pointers[i][x] * weights[i];
+          }
+          dstp[x] = static_clip<0, 255>(acc);
         }
 
         for (int i = 0; i < frames_count; ++i) {
-            src_pointers[i] += src_pitches[i];
+          src_pointers[i] += src_pitches[i];
         }
         dstp += dst_pitch;
+      }
     }
 }
 
@@ -159,31 +239,39 @@ struct WeightedClip {
 
 class Average : public GenericVideoFilter {
 public:
-    Average(std::vector<WeightedClip> clips, IScriptEnvironment* env)
-        : GenericVideoFilter(clips[0].clip), clips_(clips) {
+  Average(std::vector<WeightedClip> clips, IScriptEnvironment* env)
+    : GenericVideoFilter(clips[0].clip), clips_(clips) {
 
-        if (env->GetCPUFlags() & CPUF_SSE2) {
-            processor_ = &weighted_average_int_sse2;
+    int frames_count = clips_.size();
 
-            for (const auto& clip: clips) {
-                if (std::abs(clip.weight) > 1) {
-                    processor_ = &weighted_average_sse2;
-                    break;
-                }
-            }
-            if (clips.size() > 255) {
-                processor_ = &weighted_average_sse2;
-            }
-        } else {
-            processor_ = &weighted_average_c;
+    if (env->GetCPUFlags() & CPUF_SSE2) {
+      if (frames_count == 2)
+        processor_ = &weighted_average_int_sse2<2>;
+      else if (frames_count == 3)
+        processor_ = &weighted_average_int_sse2<3>;
+      else
+        processor_ = &weighted_average_int_sse2<0>;
+
+      for (const auto& clip : clips) {
+        if (std::abs(clip.weight) > 1) {
+          processor_ = &weighted_average_sse2;
+          break;
         }
+      }
+      if (clips.size() > 255) {
+        processor_ = &weighted_average_sse2;
+      }
     }
+    else {
+      processor_ = &weighted_average_c;
+    }
+  }
 
-    PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment *env);
+  PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment *env);
 
 private:
-    std::vector<WeightedClip> clips_;
-    decltype(&weighted_average_c) processor_;
+  std::vector<WeightedClip> clips_;
+  decltype(&weighted_average_c) processor_;
 };
 
 
